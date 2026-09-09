7cf2927a547ce60d9d4f39c1d483e84fdc73ce0e
Sushant Singh Rajput <124261283+sushantrajput@users.noreply.github.com>
2026-09-09 19:22:52 +0530

    Create App component with layout structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import Competencies from './components/Competencies';
+import CaseStudies from './components/CaseStudies';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#FBFBF9]">
+      <Hero />
+      <About />
+      <Competencies />
+      <CaseStudies />
+    </div>
+  );
+}
+
+export default App;
