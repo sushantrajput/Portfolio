519da4a6b8dbd40e960a031263e98ab59cf3d025
Sushant Singh Rajput <124261283+sushantrajput@users.noreply.github.com>
2026-09-09 20:52:16 +0530

    Refactor Tailwind config for theme properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,21 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage:{
-        deepBlue:"linear-gradient(90deg, #161B23 0%, #1C212A 25%, #1C212A 50%, #1C212A 75%, #1C212A 100%)",
-        fontBlue:"linear-gradient(50deg, #B597F6 0%, #96C6EA 100%)",
+      backgroundImage: {
+        deepBlue: "linear-gradient(90deg, #161B23 0%, #1C212A 25%, #1C212A 50%, #1C212A 75%, #1C212A 100%)",
+        fontBlue: "linear-gradient(50deg, #B597F6 0%, #96C6EA 100%)",
       },
-      colors:{
-        color1:"#6A57E0",
-        color1Hover:"#563ef4",
-        color2:"#80FFD2",
-        color3:"#161B23",
+      colors: {
+        color1: "#6A57E0",
+        color1Hover: "#563ef4",
+        color2: "#80FFD2",
+        color3: "#161B23",
       },
-      fontFamily:{
-        sora:["Sora","sans"]
+      fontFamily: {
+        sora: ["Sora", "sans-serif"]
       },
-      width:{
-        radarWidth:"40rem!important"
+      width: {
+        radarWidth: "40rem!important"
       },
     },
   },
