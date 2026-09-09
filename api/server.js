2bfd15a657931bc0b05ce904178f3e59cf20dc86
Sushant Singh Rajput <124261283+sushantrajput@users.noreply.github.com>
2026-09-09 18:38:26 +0530

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "POST") {
+    const { name, email, phone, message } = req.body;
+
+    if (!name || !email || !phone || !message) {
+      return res.status(400).json({ error: "All fields are required!" });
+    }
+
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER_NAME,
+          pass: process.env.USER_PASS,
+        },
+      });
+
+      const mailOptions = {
+        from: email,
+        to: "skrajsushant698@gmail.com", // <-- UPDATED TO YOUR EMAIL
+        subject: `Contact from ${name}`,
+        text: `You have a new message from ${name} (${email}, ${phone}):\n\n${message}`,
+      };
+
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Message sent successfully!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to send message" });
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
